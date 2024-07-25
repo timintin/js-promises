74122fab0fec0ede00f2ee9e4112de898f2dc7fe
timintin <153340367+timintin@users.noreply.github.com>
2024-07-25 19:59:34 -0400

    Delete Javascript promises directory

--- Javascript promises/Part_1_Number_Facts/single_number_fact.js
+++
@@ -1,6 +0,0 @@
-
-// Make a request to the Numbers API to get a fact about your favorite number
-fetch('http://numbersapi.com/42?json')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
